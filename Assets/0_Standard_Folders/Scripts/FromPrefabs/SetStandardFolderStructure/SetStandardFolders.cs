35d3e23bc4ada9044961dec8d722d417a478d12a
Hualonso98 <hualonso@pa.uc3m.es>
2023-06-08 12:43:53 +0200

    Add (standard folders): update standard folder package

--- Assets/0_Standard_Folders/Scripts/FromPrefabs/SetStandardFolderStructure/SetStandardFolders.cs
+++ Assets/0_Standard_Folders/Scripts/FromPrefabs/SetStandardFolderStructure/SetStandardFolders.cs
@@ -1,3 +1,20 @@
+/**************************************************************************************
+* Copyright (c) 2023. Universidad Carlos III de Madrid. Todos los derechos reservados *
+*                                                                                     *
+* Authors: Hugo Alonso Cámara, Edwin Daniel Oña, Carlos Balaguer, and Alberto Jardón  *
+*                                                                                     *
+* Descripción del proyecto:                                                           *
+* [Desarrollo de Serious Games orientados a la rehabilitación de miembro superior     *
+*  mediante el uso de dispositivos de tracking de manos y antebrazo como Ultraleap,   *
+*  complementado con la inmersión en Realidad Virtual con Oculus]                     *
+*                                                                                     *
+*                                                                                     *
+* Descripción del juego:                                                              *
+* [Lanzador que centraliza la gestión de pacientes/usuarios y se encarga de           *
+*  la apertura del lanzamiento del resto de juegos]                                   *
+*                                                                                     *
+***************************************************************************************/
+
 using System;
 using System.Collections;
 using System.Collections.Generic;
@@ -23,23 +40,35 @@ public class SetStandardFolders : MonoBehaviour
         folders.Add("Models/Hands");
         folders.Add("Prefabs");
         folders.Add("Prefabs/Hands");
+        folders.Add("Prefabs/FromScenes");
+        folders.Add("Prefabs/UI");
         folders.Add("Scenes");
         folders.Add("Scripts");
+        folders.Add("Scripts/Protocol");
         folders.Add("Scripts/Data");
         folders.Add("Scripts/Data/GameData");
         folders.Add("Scripts/Data/Patients");
         folders.Add("Scripts/Data/Static");
         folders.Add("Scripts/FromScenes");
+        folders.Add("Scripts/FromPrefabs");
         folders.Add("Scripts/Hands");
         folders.Add("Scripts/Utilities");
         folders.Add("Scripts/Utilities/UI");
+        folders.Add("Scripts/Utilities/UI/Logos");
+        folders.Add("Scripts/Utilities/UI/Texts-Labels");
         folders.Add("Sounds");
         folders.Add("Sounds/EventsSounds");
         folders.Add("Sounds/Themes");
         folders.Add("Sprites");
         folders.Add("Sprites/Menu_UI");
+        folders.Add("Sprites/Menu_UI/StandardButtons");
+        folders.Add("Sprites/Menu_UI/Logos");
+        folders.Add("Sprites/Menu_UI/Panels");
+        folders.Add("Sprites/Captures");
         folders.Add("Sprites/Resources");
-        folders.Add("Sprites/StandardButtons");
+        folders.Add("Multimedia");
+        folders.Add("Multimedia/Videos");
+        folders.Add("Multimedia/Gifs");
         folders.Add("Tests");
 
         CreateFolders();
